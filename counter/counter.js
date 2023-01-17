b80c7c9e6b03c1bf96786ecb6a3ac2eb251406db
Vishnu.C P <115421837+vishnu-13-97@users.noreply.github.com>
2023-01-17 22:21:28 +0530

    Update counter.js

--- counter/counter.js
+++ counter/counter.js
@@ -5,22 +5,16 @@ document.getElementById("btndecrease").onclick= function () {
   count--
   document.getElementById("num").innerHTML=count;
   document.getElementById("num").style.color = "red";
-  
-}
+  }
+
 document.getElementById("btnincrease").onclick= function () {
   count++;
   document.getElementById("num").innerHTML=count;
   document.getElementById("num").style.color = "green";
-  
-}
+  }
+
 document.getElementById("btnreset").onclick= function () {
   count=0;
   document.getElementById("num").innerHTML=count;
   document.getElementById("num").style.color = "black";
-  
-}
-document.getElementById("btndecrease").onclick = function(){
-  
-  document.getElementById("btndecrease").style.color = "red";
-  
-}
+  }
